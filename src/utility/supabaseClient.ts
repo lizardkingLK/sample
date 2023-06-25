72a920349e5bb7a7d76c5e521ffb3b00337c65e7
lizardkingLK <chansanfdo@gmail.com>
2023-06-25 05:22:11 +0000

    added environment variables. updated readme.md

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,10 +1,6 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://ufvzftlgaxdlqbhzrqpb.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmdnpmdGxnYXhkbHFiaHpycXBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc2MzAyMTcsImV4cCI6MjAwMzIwNjIxN30.6onjjiO6X6MRfk5dwG5nCxWtbyshzgI4lzJ8RAfonUw";
-
-export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
+export const supabaseClient = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_KEY!, {
   db: {
     schema: "public",
   },
